c14e0c0ac1ca891456744fd6aefd8c1f6e9813c0
James <37276661+capjamesg@users.noreply.github.com>
2025-08-30 16:42:47 +0100

    restructure project

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "includes/head.html" ?>
+<body>
+    <?php $title = "All Stations";
+        include "includes/nav.php"; ?>
+    <section id="home">
+        <h1>rainfall.scot</h1>
+        <p>Search for rainfall by month at your nearest station:</p>
+        <search>
+            <form action="rainfall.php" method="get">
+                <input type="text" id="station" name="station" placeholder="e.g. Harelaw">
+                <input type="submit" value="Search">
+            </form>
+        </search>
+    </section>
+</body>
+</html>
